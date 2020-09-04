5c04173ba41e9c0137413381deb5e35d9bfe3d42
SFernandoS <SFernandoS@.github.com>
2020-09-03 23:16:26 -0300

    checkio (Days Between)

---
+++ checkio/Home/Days_Between.py
@@ -0,0 +1,11 @@
+""" 
+    https://py.checkio.org/mission/days-diff/solve/
+"""
+
+from datetime import date
+
+
+def days_diff(a, b):
+    diferença = date(a[0], a[1], a[2]) - date(b[0], b[1], b[2])
+
+    return abs(diferença.days)
